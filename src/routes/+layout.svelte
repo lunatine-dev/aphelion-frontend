e549d15cc3a73ad331b8d8fa80ecc6bbc385027e
Lunatine <github@lunatine.dev>
2025-12-01 14:06:48 +0000

    feat(toast): svelte-sonner added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script>
     import { ModeWatcher } from "mode-watcher";
+    import { Toaster } from "$lib/components/ui/sonner/index.js";
     import "../app.css";
     let { children } = $props();
 </script>
+
+<Toaster richColors position="bottom-center" />
 
 <ModeWatcher />
 
